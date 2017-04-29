fb663a7c439bdb72dadebb0a077f6f58217e32c8
Kees van der vlis <kees@slowthinkers.net>
2017-04-29 10:49:28 +0200

    Initial Commit

---
+++ egg timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  egg timer
+//
+//  Created by Kees van der vlis on 29/04/2017.
+//  Copyright © 2017 Kees van der vlis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
