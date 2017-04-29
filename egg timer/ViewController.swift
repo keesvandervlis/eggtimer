45ad956ffd1be0b37a1f7f09524e322277602e96
Kees van der vlis <kees@slowthinkers.net>
2017-04-29 12:55:20 +0200

    working egg timer

--- egg timer/ViewController.swift
+++ egg timer/ViewController.swift
@@ -9,10 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = Timer()
+    var startTijd = 300
+    
+    func updatetimerViewLabel ( nieuweTijd :Int) {
+        let nieuweTijd = nieuweTijd
+        timerViewLabel.text = String(nieuweTijd)
+    }
 
+    func processTimer() {
+        if startTijd >= 1 {
+            startTijd = startTijd - 1
+            updatetimerViewLabel(nieuweTijd: startTijd)
+        }
+    }
+    
+    @IBAction func playPressed(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.processTimer), userInfo: nil, repeats: true)
+    }
+    
+    
+    @IBAction func pausePressed(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    @IBAction func minusTenPressed(_ sender: Any) {
+        if startTijd > 10 {
+            startTijd = startTijd - 10
+            updatetimerViewLabel(nieuweTijd: startTijd)
+        }
+    }
+    
+    @IBAction func resetPressed(_ sender: Any) {
+        startTijd = 300
+        updatetimerViewLabel(nieuweTijd: startTijd)
+    }
+    
+    @IBAction func plusTenPressed(_ sender: Any) {
+        startTijd = startTijd + 10
+        updatetimerViewLabel(nieuweTijd: startTijd)
+    }
+    
+    @IBOutlet weak var timerViewLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+     
+            updatetimerViewLabel(nieuweTijd: startTijd)
     }
 
     override func didReceiveMemoryWarning() {
